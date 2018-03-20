14f5304cc9ed698679e9bb0da3f55c38ad19f39c
Wagner Nogueira <wagnogueira19@hotmail.com>
2018-03-20 00:02:23 -0300

    send sms with twilio

---
+++ TwilioSms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace TwilioSms
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SendSms().Wait();
+      Console.Write("Press any key to continue.");
+      Console.ReadKey();
+    }
+
+    static async Task SendSms()
+    {
+      var accountSid = "AC568ddd31702c5f008624ed1093819380bf";
+      var authToken = "087bec34a199493f9da5bfb69354a3d9dlakd";
+
+      TwilioClient.Init(accountSid, authToken);
+
+      var numeros = new List<string>();
+      numeros.Add("+5562991350203");
+
+      foreach(var n in numeros)
+      {
+        var message = await MessageResource.CreateAsync(
+         to: new PhoneNumber(n),
+         from: new PhoneNumber("+18339233251"),
+         body: "Testando envio de sms.");
+
+        Console.WriteLine(message.Sid);
+      }
+    }
+
+  }
+}
